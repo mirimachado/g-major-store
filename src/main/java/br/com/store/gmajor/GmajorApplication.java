78712bd1744e3fac27502ac5745f0e92f5b5e8f4
mirimachado <mirii.machadoo.c@gmail.com>
2024-07-24 15:54:09 -0300

    #1 - Adicionando repositório

---
+++ src/main/java/br/com/store/gmajor/GmajorApplication.java
@@ -0,0 +1,13 @@
+package br.com.store.gmajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmajorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmajorApplication.class, args);
+	}
+
+}
